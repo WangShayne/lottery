3fa2c5a4eaf650d5ccdfeeac988219ad238f88be
shayne <1614565666@qq.com>
2019-07-12 10:46:22 +0800

    init commit

---
+++ contracts/lottery.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.4.24;
+
+contract Lotty {
+    uint256 public total ;
+
+    con
+
+    // 返回奖池总金额
+    function getTotal() public view returns(uint256){
+        return total;
+    }
+
+    //
+
+
+
+}
