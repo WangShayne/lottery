556a3d95ef5315366de17fb791ecedd3795b9981
shayne <1614565666@qq.com>
2019-07-19 14:19:14 +0800

    合约v1 基础结构

--- contracts/lottery.sol
+++ contracts/lottery.sol
@@ -1,17 +1,31 @@
 pragma solidity ^0.4.24;
 
 contract Lotty {
-    uint256 public total ;
+    // 1. 管理员:负责开奖和提奖
+    address public manager;
+    // 2. 彩民池: address[] players
+    address[] public players;
+    // 3. 当前期数: round,每期结束后加一
+    uint256 public round;
 
-    con
-
-    // 返回奖池总金额
-    function getTotal() public view returns(uint256){
-        return total;
+    constructor() public{
+        manager = msg.sender;
     }
 
-    //
+    // 实现投注函数
+    // 1.每个人可以投多次
+    function play() public payable{
+        require(msg.value == 1 ether);
+        // 2.把参与者加入到彩民池中
+        players.push(msg.sender);
+    }
 
-
+    function getBalance() public view returns(uint256){
+        return address(this).balance;
+    }
+    
+    function getPlayers() public view returns(address[]){
+        return players;
+    }
 
 }
